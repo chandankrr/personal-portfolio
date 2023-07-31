b7cbbc75e0ffe49c6ba5d3fa18d601dd9cda3ead
Chandan Kumar <chandankrr.91@gmail.com>
2023-07-31 14:14:24 +0530

    designed the navigation bar of intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import cloudSoft from '../../images/cloud-soft.png';
+import cloud from '../../images/cloud.png';
+
+import NavigationBar from './navigation-bar/NavigationBar';
+import './style.scss';
 
 const Intro = () => {
-  return <div>Intro</div>;
+  return (
+    <div className="intro-section">
+      <div className="vector-bg" id="parallax"></div>
+      <img src={cloud} alt="cloud" className="cloud" />
+      <img src={cloudSoft} alt="cloud-soft" className="cloud-soft" />
+      <div className="content">
+        <NavigationBar />
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
---
+++ src/components/intro/navigation-bar/NavigationBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../../../images/logo.png';
+import CallToAction from '../shared/CallToAction/CallToAction';
+import './style.scss';
+
+const NavigationBar = () => {
+  return (
+    <div className="top-navigation-bar">
+      <div className="app-logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="navigation">
+        <span className="navigation-item">Skills</span>
+        <span className="navigation-item">Portfolio</span>
+        <span className="navigation-item">Blogs & Articles</span>
+        <CallToAction text="Contact me" />
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
