36051b35d8b5547fc8ceaf66e95d123d54161e11
Chandan Kumar <chandankrr.91@gmail.com>
2023-08-13 12:27:04 +0530

    added responsive design to intro section

--- src/components/intro/navigation-bar/NavigationBar.jsx
+++ src/components/intro/navigation-bar/NavigationBar.jsx
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CgMenuRight } from 'react-icons/cg';
+import { IoMdClose } from 'react-icons/io';
 import logo from '../../../images/logo.png';
 import CallToAction from '../../shared/CallToAction/CallToAction';
 import { scrollToSection } from '../../utils/helpers';
 import './style.scss';
 
 const NavigationBar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const menuItemClickHandler = (section) => {
+    setMobileMenu(!mobileMenu);
+    scrollToSection(section);
+  };
+
   return (
     <div className="top-navigation-bar">
       <div className="app-logo">
         <img src={logo} alt="logo" />
       </div>
-      <div className="navigation">
+      <div className="mobile-menu" onClick={() => setMobileMenu(!mobileMenu)}>
+        {mobileMenu ? (
+          <IoMdClose size={24} color="#ffffff" />
+        ) : (
+          <CgMenuRight size={24} color="#ffffff" />
+        )}
+      </div>
+      <div className={`navigation ${mobileMenu ? 'active' : ''}`}>
         <span
           className="navigation-item"
-          onClick={() => scrollToSection('skills')}
+          onClick={() => menuItemClickHandler('skills')}
         >
           Skills
         </span>
         <span
           className="navigation-item"
-          onClick={() => scrollToSection('portfolio')}
+          onClick={() => menuItemClickHandler('portfolio')}
         >
           Portfolio
         </span>
         <span
           className="navigation-item"
-          onClick={() => scrollToSection('blogs')}
+          onClick={() => menuItemClickHandler('blogs')}
         >
           Blogs & Articles
         </span>
         <CallToAction
           text="Contact me"
-          action={() => scrollToSection('contact')}
+          action={() => menuItemClickHandler('contact')}
         />
       </div>
     </div>
